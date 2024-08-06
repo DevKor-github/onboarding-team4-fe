f3d73a657e495c52d1ba51c7ac804fa8bcd5ae34
ejun <jjoonleo@gmail.com>
2024-08-06 14:48:09 +0900

    Doc: Add comment to APIUtils.ts

--- src/utils/APIUtils.ts
+++ src/utils/APIUtils.ts
@@ -3,7 +3,7 @@ import axios, {
   AxiosRequestConfig,
 } from "axios";
 
-export interface APIResponse<T> {
+interface APIResponse<T> {
   message: string;
   data: T;
 }
@@ -15,7 +15,20 @@ const client: AxiosInstance = axios.create({
   },
 });
 
-const handleAxiosError = async <T>(callback: <T>() => Promise<APIResponse<T>>):Promise<T> => {
+/**
+ * Returns the data from the APIResponse if the request is successful and when an error occurs, it logs the error
+ * 
+ * @remarks
+ * This function is used to handle meta datas in APIResponse and return the data when the request is successful
+ * When an error occurs, it handle the error in three cases: 
+ * 1. The request was made and the server responded with a status code
+ * 2. The request was made but no response was received
+ * 3. Something happened in setting up the request that triggered an Error
+ * 
+ * @param callback - http request function that returns a APIResponse in a promise
+ * @returns APIResponse.data returned by the callback
+ */
+const handleAxiosError = async <T>(callback: () => Promise<APIResponse<T>>):Promise<T> => {
   try {
     const response : APIResponse<T> = await callback();
     console.log(response.message);
@@ -23,12 +36,15 @@ const handleAxiosError = async <T>(callback: <T>() => Promise<APIResponse<T>>):P
   } catch (error: unknown) {
     if (axios.isAxiosError(error)) {
       if (error.response) {
+        // The request was made and the server responded with a status code
         console.error(error.response.data);
         console.error(error.response.status);
         console.error(error.response.headers);
       } else if (error.request) {
+        // The request was made but no response was received
         console.error(error.request);
       } else {
+        // Something happened in setting up the request that triggered an Error
         console.error("Error", error.message);
       }
       console.error(error.config);
@@ -37,43 +53,74 @@ const handleAxiosError = async <T>(callback: <T>() => Promise<APIResponse<T>>):P
   }
 }
 
+/**
+ * Makes a GET request to the specified URL and returns the data field inside the APIResponse
+ * 
+ * @param url - The URL to which the request is made
+ * @param config - The config object that is used to make the request
+ * @returns data field inside the APIResponse
+ */
 export const getData = async <T>(
   url: string,
   config?: AxiosRequestConfig
 ): Promise<T> => {
-  return await handleAxiosError<T>(async <T>()=>{
+  return await handleAxiosError<T>(async ()=>{
     const response = await client.get<APIResponse<T>>(url, config);
     return response.data;
   });
 };
 
-export const postData = async <T>(
+/**
+ * Makes a POST request to the specified URL and returns the data field inside the APIResponse
+ * 
+ * @param url - The URL to which the request is made
+ * @param data - The data to be sent to the server
+ * @param config - The config object that is used to make the request
+ * @returns data field inside the APIResponse
+ */
+export const postData = async <T, U>(
   url: string,
-  data?: unknown,
+  data?: U,
   config?: AxiosRequestConfig
 ): Promise<T> => {
-  return handleAxiosError<T>(async <T>()=>{
+  return await handleAxiosError<T>(async ()=>{
     const response = await client.post<APIResponse<T>>(url, data, config);
     return response.data;
   })
 };
 
-export const putData = async <T>(
+
+/**
+ * Makes a PUT request to the specified URL and returns the data field inside the APIResponse
+ * 
+ * @param url - The URL to which the request is made
+ * @param data - The data to be sent to the server
+ * @param config - The config object that is used to make the request
+ * @returns data field inside the APIResponse
+ */
+export const putData = async <T, U>(
   url: string,
-  data?: unknown,
+  data?: U,
   config?: AxiosRequestConfig
 ): Promise<T> => {
-  return handleAxiosError<T>(async <T>()=>{
+  return await handleAxiosError<T>(async ()=>{
     const response = await client.put<APIResponse<T>>(url, data, config);
     return response.data;
   });
 };
 
+/**
+ * Makes a DELETE request to the specified URL and returns the data field inside the APIResponse
+ * 
+ * @param url - The URL to which the request is made
+ * @param config - The config object that is used to make the request
+ * @returns data field inside the APIResponse
+ */
 export const deleteData = async <T>(
   url: string,
   config?: AxiosRequestConfig
 ): Promise<T> => {
-  return handleAxiosError<T>(async <T>()=>{
+  return await handleAxiosError<T>(async ()=>{
     const response = await client.delete<APIResponse<T>>(url, config);
     return response.data;
   });
