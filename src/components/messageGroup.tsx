510715a99df0358bc272f3366e0707a35b4d479c
ejun <jjoonleo@gmail.com>
2024-08-17 19:09:50 +0900

    Refactor: Add MessageGroup component
    Add MessageGroup component to handle the display of a group of messages in a chat. This component takes in the type of message group (left or right) and the data for the chat list. It renders the appropriate UI based on the type and displays the messages using the MessageBubble component.

---
+++ src/components/messageGroup.tsx
@@ -0,0 +1,31 @@
+import { ChatList } from '../api/models/chatModel';
+import MessageBubble from './messageBubble';
+import { MessageBubbleType, MessageGroupType } from './messageBubbleType';
+
+function MessageGroup({type, data}: {type: MessageGroupType, data: ChatList}) {
+  const className = {
+    [MessageGroupType.LEFT]: {
+      row: 'justify-start',
+    },
+    [MessageGroupType.RIGHT]: {
+      row: 'justify-end',
+    },
+  }[type];
+  
+  return (
+    <div className={`flex flex-row w-full flex-1 ${className["row"]}`}>
+      {type === MessageGroupType.LEFT ?  <div className='w-8 h-8'>
+        <img src={data.img} alt="profile img" />
+      </div> : null}
+      <div className={`flex flex-col w-fit flex-1 max-w-64`}>
+        
+        {data.chats.map((chat, index) => (
+          <MessageBubble type={ index === 0 && type === MessageGroupType.LEFT ? MessageBubbleType.LEFT : MessageBubbleType.RIGHT} senderName={data.senderName} data={chat}/>
+        ))}
+      </div>
+    </div>
+    
+  );
+}
+
+export default MessageGroup;
