42039abe8cbc8732effe12db0cccc280bdb23c7e
ejun <jjoonleo@gmail.com>
2024-08-19 21:40:34 +0900

    chore: Update tailwind.config.js with new color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primary: "#3189F0",
+      label : {
+        300 : "#2C2C2E",
+        200 : "#3D3D3D",
+        100 : "#666668",
+      },
+      background : "#FFF",
+      onBackground : "#F2F2F7",
+    },
     extend: {},
   },
   plugins: [],
