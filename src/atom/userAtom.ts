2e411b139da125d01dd047622bfb2d81daf3f15d
34selen <73600142+34selen@users.noreply.github.com>
2024-08-26 17:44:49 +0900

    Update userAtom.ts

--- src/atom/userAtom.ts
+++ src/atom/userAtom.ts
@@ -4,5 +4,5 @@ import { User } from '../api/models/User';
 export const userAtom = atomWithStorage<User|null>('user', null);
 
 
-export const userTokenAtom = atomWithStorage<string|null>('userToken', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmNhZTBjY2NkMzcxYmE5OWZmNTFiMDYiLCJ1c2VyTmljayI6IkIiLCJwcm9maWxlSW1hZ2UiOiIiLCJzdWIiOiJhc2RmNTY3OCIsImlhdCI6MTcyNDY1MzIxMywiZXhwIjoxNzI0NjYwNDEzfQ.nFJOtISdvaxkVcteRowZ5oLG0RPDTU3Wkzx1TE-ilHg');
+export const userTokenAtom = atomWithStorage<string|null>('userToken', null);
 
