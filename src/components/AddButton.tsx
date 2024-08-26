3ac1e0056f9c98fc165be77f728f0472b20b3294
ejun <jjoonleo@gmail.com>
2024-08-26 16:09:03 +0900

    refactor: Optimize image imports in components
    - Replace direct image imports with imported variables
    - Update image paths in AddButton, AppBar, MessageField, UserWithImg, and MessageBubble components

--- src/components/AddButton.tsx
+++ src/components/AddButton.tsx
@@ -1,6 +1,8 @@
+import plusImage from '../assets/plus.svg';
+
 function AddButton({onClick}: {onClick: () => void}) {
   return <button className='size-12 rounded-full bg-primary p-3' onClick={onClick}>
-    <img src='/src/assets/plus.svg' alt='add' />
+    <img src={plusImage} alt='add' />
   </button>;
 }
 
--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,8 +1,10 @@
+import devkorImage from '../assets/devkor.svg';
+
 function AppBar() {
   return (
     <div className='flex flex-row justify-start items-center px-6 py-[0.94rem] gap-1'>
       <div>
-        <img className='h-4 w-4' src='/src/assets/devkor.svg' alt='logo'/>
+        <img className='h-4 w-4' src={devkorImage} alt='logo'/>
       </div>
       <h2 className='font-bold text-md text-center text-label-200'>DEVKOR</h2>
     </div>
